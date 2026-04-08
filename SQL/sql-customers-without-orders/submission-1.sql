78351b0accf78bbb667b48cfac8e9909c906e8db
AshKap32 <96267943+AshKap32@users.noreply.github.com>
2026-04-08 15:13:24 -0700

    Add: sql-customers-without-orders - submission-1

---
+++ SQL/sql-customers-without-orders/submission-1.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT c.name
+FROM customers AS c
+LEFT JOIN orders AS o 
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
+
+-- SELECT c.name 
+-- FROM customers AS c 
+-- LEFT JOIN orders AS o ON c.id = o.customer_id 
+-- WHERE o.customer_id IS NULL;
