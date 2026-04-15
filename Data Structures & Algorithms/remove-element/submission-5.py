27482ce5d11344181a9b4409a7861fe6805aa66e
AshKap32 <96267943+AshKap32@users.noreply.github.com>
2026-04-15 15:24:28 -0700

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        # Can theoretically count the freq of val, then every time its then mvove it to the end.
+
+        # LEft and right pointer, left pointer will scan the list for the val, rightpointer find the next good spot to swap
+        # If the right pointer sees the val, then we decrement the right pointer
+
+        # Then return len(nums) - r to be k
+
+        r = len(nums) - 1
+
+        # Get count of val
+        freq = defaultdict(int)
+        for num in nums:
+            if num == val:
+                freq[num] += 1
+
+        for l, num in enumerate(nums):
+            # if the right pointer passes the left break out of the loop
+            if l > r:
+                break
+
+            # Find next valid swap value
+            while nums[r] == val and r > 0:
+                r -= 1
+
+            if num != val:
+                continue
+            else:
+                # Found val, swap them
+
+                nums[l], nums[r] = nums[r], nums[l]
+                r -= 1
+
+            
+        return r + 1
