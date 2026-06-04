67031d3bf4cb8a800daf8ca211c330ebcb1a6974
AshKap32 <96267943+AshKap32@users.noreply.github.com>
2026-06-04 15:51:29 -0700

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Create a hashmap to map the opening brackets to
+        key = {'}':'{', ']':'[', ')': '('}
+
+        # Create a stack to add the opening brackets to
+        stack = []
+
+        # once we get to a closing bracket  we chcek to see if the last value [-1] is == to the closing brracket
+        # we are currently on, that ensures the opening and closing brackets are in the rigth order and correct type
+        # at the end ifthe stack is empty we return true, assuming we havent returned false in the middle of it
+
+        for bracket in s:
+            # O(1) lookup and checks if its an oepnign ot closing bracket
+            if bracket in key:
+                # checks to see if the opening bracket in the stack == the closing brackets key (opening bracket)
+                # Checks for order here as well as right type of bracket, also check to see if there is something
+                # in the stack, becuase if there is now that means we have an extra closing bracket and no opening 
+                # to match that.
+                if stack and key[bracket] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False   
+            else:
+                # Add opening bracket into the stack
+                stack.append(bracket)
+        
+        return stack == []
+
