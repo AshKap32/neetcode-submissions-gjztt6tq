fe2a32b64997a57eaafd550eda945ee9525a9bfc
AshKap32 <96267943+AshKap32@users.noreply.github.com>
2026-06-10 18:30:38 -0700

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # The intuition here is we can find all anagrams by doing a one pass and creating a sort
+        # Of the words and make that the key of the map if we find another sorted word to be similar we can add that into our list
+
+        # anagrams = defaultdict(list)
+
+        # for word in strs:
+        #     # Joining the word since sorted(word) returns a list of characters
+        #     sorted_w = "".join(sorted(word))
+        #     anagrams[sorted_w].append(word)
+
+        # # Converting the default dict values into an array of arrays
+
+        # return list(anagrams.values())
+
+        anagrams = defaultdict(list)
+
+        for string in strs:
+            sorted_w = str(sorted(string))
+
+            anagrams[sorted_w].append(string)
+        
+        return list(anagrams.values())
